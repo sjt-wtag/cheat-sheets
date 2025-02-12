48e8635c9ebfbb9f404d1f648867667ed97d2d60
Sumaya Jahan <sumayajahan@Sumayas-Mac-mini.local>
2025-02-12 13:35:35 +0600

    closure vs not closure

--- closures.js
+++ closures.js
@@ -1,19 +1,30 @@
-// function temporary() {
-//   let counter = 0;
+// closure
+var num1 = 2;
 
-//   return function() {  
-//     counter += 1;
-//   }
+var sum = function () {
+  var num2 = 3;
+  var num = 6;
 
-// }
+  return function () {
+    return num1 + num2; // this will be closure as num2 is called
+  };
+};
 
-// const add = temporary(); 
-// console.dir(add)
-// add();
-// console.dir(add)
-// add();
-// console.dir(add)
-// add();
+var myFunc = sum();
+console.dir(myFunc);
 
+// not closure
+var num1 = 2;
+var num3 = 9;
 
+var sum = function () {
+  var num2 = 3;
+  var num = 6;
 
+  return function () {
+    return num1 + num3;
+  };
+};
+
+var myFunc = sum();
+console.dir(myFunc);
